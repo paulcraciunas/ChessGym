00b65115a3c39a4ab5effb58104a4be71183c9cd
Paul.Craciunas <paul.craciunas@team.bumble.com>
2024-08-15 16:58:05 +0100

    Added custom colours for the board

--- app/src/main/java/com/paulcraciunas/chessgym/ui/theme/Theme.kt
+++ app/src/main/java/com/paulcraciunas/chessgym/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.paulcraciunas.chessgym.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -21,16 +21,6 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -50,9 +40,16 @@ fun ChessGymTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    val customColorsPalette = if (darkTheme) DarkCustomColorPalette
+    else LightCustomColorPalette
+
+    CompositionLocalProvider(
+        LocalCustomColorPalette provides customColorsPalette
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
