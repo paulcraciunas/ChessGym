8ccfa08c8ca51ae4e516d54b88f12b1dbd113c0c
Paul.Craciunas <paul.craciunas@team.bumble.com>
2024-08-20 19:45:54 +0100

    Added an implementation for castling kingSide and queenSide

---
+++ app/src/main/java/com/paulcraciunas/chessgym/game/plies/CastlePly.kt
@@ -0,0 +1,36 @@
+package com.paulcraciunas.chessgym.game.plies
+
+import com.paulcraciunas.chessgym.game.Side
+import com.paulcraciunas.chessgym.game.board.Board
+import com.paulcraciunas.chessgym.game.board.File
+import com.paulcraciunas.chessgym.game.board.Locus
+import com.paulcraciunas.chessgym.game.board.Piece
+import com.paulcraciunas.chessgym.game.board.Rank
+
+class CastlePly(override val turn: Side, val type: Type) : Ply {
+    override val piece = Piece.King
+    override val from = type.from(turn)
+    override val to = type.end(turn)
+
+    override fun exec(on: Board) {
+        on.move(from = from, to = to, turn = turn)
+        on.move(from = type.rook(turn), to = type.pass(turn), turn = turn)
+    }
+
+    override fun undo(on: Board) {
+        on.move(from = to, to = from, turn = turn)
+        on.move(from = type.pass(turn), to = type.rook(turn), turn = turn)
+    }
+
+    enum class Type(val passFile: File, val endFile: File, val rookFile: File) {
+        KingSide(passFile = File.f, endFile = File.g, rookFile = File.h),
+        QueenSide(passFile = File.d, endFile = File.c, rookFile = File.a);
+
+        fun from(turn: Side): Locus = Locus(File.e, rank(turn))
+        fun pass(turn: Side): Locus = Locus(passFile, rank(turn))
+        fun end(turn: Side): Locus = Locus(endFile, rank(turn))
+        fun rook(turn: Side): Locus = Locus(rookFile, rank(turn))
+
+        private fun rank(turn: Side): Rank = if (turn == Side.WHITE) Rank.`1` else Rank.`8`
+    }
+}
---
+++ app/src/test/java/com/paulcraciunas/chessgym/game/plies/CastlePlyTest.kt
@@ -0,0 +1,110 @@
+package com.paulcraciunas.chessgym.game.plies
+
+import com.paulcraciunas.chessgym.game.Side
+import com.paulcraciunas.chessgym.game.board.Board
+import com.paulcraciunas.chessgym.game.board.File
+import com.paulcraciunas.chessgym.game.board.Locus
+import com.paulcraciunas.chessgym.game.board.Piece
+import com.paulcraciunas.chessgym.game.board.Rank
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CastlePlyTest {
+    private val on = Board()
+
+    @Test
+    fun `GIVEN white moving WHEN castling kingSide for THEN the king and rook move correctly`() {
+        on.add(piece = Piece.King, side = Side.WHITE, at = Locus(File.e, Rank.`1`))
+        on.add(piece = Piece.Rook, side = Side.WHITE, at = Locus(File.h, Rank.`1`))
+        val ply = CastlePly(turn = Side.WHITE, type = CastlePly.Type.KingSide)
+
+        ply.exec(on)
+
+        assertTrue(on.has(Piece.King, Side.WHITE, Locus(File.g, Rank.`1`)))
+        assertTrue(on.has(Piece.Rook, Side.WHITE, Locus(File.f, Rank.`1`)))
+    }
+
+    @Test
+    fun `GIVEN white moving WHEN undoing kingSide castle THEN the king and rook move back`() {
+        on.add(piece = Piece.King, side = Side.WHITE, at = Locus(File.g, Rank.`1`))
+        on.add(piece = Piece.Rook, side = Side.WHITE, at = Locus(File.f, Rank.`1`))
+        val ply = CastlePly(turn = Side.WHITE, type = CastlePly.Type.KingSide)
+
+        ply.undo(on)
+
+        assertTrue(on.has(Piece.King, Side.WHITE, Locus(File.e, Rank.`1`)))
+        assertTrue(on.has(Piece.Rook, Side.WHITE, Locus(File.h, Rank.`1`)))
+    }
+
+    @Test
+    fun `GIVEN white moving WHEN castling queenSide for THEN the king and rook move correctly`() {
+        on.add(piece = Piece.King, side = Side.WHITE, at = Locus(File.e, Rank.`1`))
+        on.add(piece = Piece.Rook, side = Side.WHITE, at = Locus(File.a, Rank.`1`))
+        val ply = CastlePly(turn = Side.WHITE, type = CastlePly.Type.QueenSide)
+
+        ply.exec(on)
+
+        assertTrue(on.has(Piece.King, Side.WHITE, Locus(File.c, Rank.`1`)))
+        assertTrue(on.has(Piece.Rook, Side.WHITE, Locus(File.d, Rank.`1`)))
+    }
+
+    @Test
+    fun `GIVEN white moving WHEN undoing queenSide castle THEN the king and rook move back`() {
+        on.add(piece = Piece.King, side = Side.WHITE, at = Locus(File.c, Rank.`1`))
+        on.add(piece = Piece.Rook, side = Side.WHITE, at = Locus(File.d, Rank.`1`))
+        val ply = CastlePly(turn = Side.WHITE, type = CastlePly.Type.QueenSide)
+
+        ply.undo(on)
+
+        assertTrue(on.has(Piece.King, Side.WHITE, Locus(File.e, Rank.`1`)))
+        assertTrue(on.has(Piece.Rook, Side.WHITE, Locus(File.a, Rank.`1`)))
+    }
+
+    @Test
+    fun `GIVEN black moving WHEN castling kingSide for THEN the king and rook move correctly`() {
+        on.add(piece = Piece.King, side = Side.BLACK, at = Locus(File.e, Rank.`8`))
+        on.add(piece = Piece.Rook, side = Side.BLACK, at = Locus(File.h, Rank.`8`))
+        val ply = CastlePly(turn = Side.BLACK, type = CastlePly.Type.KingSide)
+
+        ply.exec(on)
+
+        assertTrue(on.has(Piece.King, Side.BLACK, Locus(File.g, Rank.`8`)))
+        assertTrue(on.has(Piece.Rook, Side.BLACK, Locus(File.f, Rank.`8`)))
+    }
+
+    @Test
+    fun `GIVEN black moving WHEN undoing kingSide castle THEN the king and rook move back`() {
+        on.add(piece = Piece.King, side = Side.BLACK, at = Locus(File.g, Rank.`8`))
+        on.add(piece = Piece.Rook, side = Side.BLACK, at = Locus(File.f, Rank.`8`))
+        val ply = CastlePly(turn = Side.BLACK, type = CastlePly.Type.KingSide)
+
+        ply.undo(on)
+
+        assertTrue(on.has(Piece.King, Side.BLACK, Locus(File.e, Rank.`8`)))
+        assertTrue(on.has(Piece.Rook, Side.BLACK, Locus(File.h, Rank.`8`)))
+    }
+
+    @Test
+    fun `GIVEN black moving WHEN castling queenSide for THEN the king and rook move correctly`() {
+        on.add(piece = Piece.King, side = Side.BLACK, at = Locus(File.e, Rank.`8`))
+        on.add(piece = Piece.Rook, side = Side.BLACK, at = Locus(File.a, Rank.`8`))
+        val ply = CastlePly(turn = Side.BLACK, type = CastlePly.Type.QueenSide)
+
+        ply.exec(on)
+
+        assertTrue(on.has(Piece.King, Side.BLACK, Locus(File.c, Rank.`8`)))
+        assertTrue(on.has(Piece.Rook, Side.BLACK, Locus(File.d, Rank.`8`)))
+    }
+
+    @Test
+    fun `GIVEN black moving WHEN undoing queenSide castle THEN the king and rook move back`() {
+        on.add(piece = Piece.King, side = Side.BLACK, at = Locus(File.c, Rank.`8`))
+        on.add(piece = Piece.Rook, side = Side.BLACK, at = Locus(File.d, Rank.`8`))
+        val ply = CastlePly(turn = Side.BLACK, type = CastlePly.Type.QueenSide)
+
+        ply.undo(on)
+
+        assertTrue(on.has(Piece.King, Side.BLACK, Locus(File.e, Rank.`8`)))
+        assertTrue(on.has(Piece.Rook, Side.BLACK, Locus(File.a, Rank.`8`)))
+    }
+}
