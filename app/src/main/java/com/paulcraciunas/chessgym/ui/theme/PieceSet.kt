c670f72f1166785038ac0f46bc5ead8022cdf55b
Paul.Craciunas <paul.craciunas@team.bumble.com>
2024-12-22 17:00:49 +0000

    Added local composition for the piece icons so we can more easily theme them later on

---
+++ app/src/main/java/com/paulcraciunas/chessgym/ui/theme/PieceSet.kt
@@ -0,0 +1,50 @@
+package com.paulcraciunas.chessgym.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.paulcraciunas.chessgym.R
+
+@Immutable
+data class Pieces(
+    val pawn: Int = error("Undefined"),
+    val rook: Int = error("Undefined"),
+    val bishop: Int = error("Undefined"),
+    val knight: Int = error("Undefined"),
+    val queen: Int = error("Undefined"),
+    val king: Int = error("Undefined"),
+)
+
+@Immutable
+data class PieceSet(
+    val white: Pieces = Pieces(),
+    val black: Pieces = Pieces()
+)
+
+val DefaultWhitePieces = Pieces(
+    pawn = R.drawable.pawn_white,
+    rook = R.drawable.rook_white,
+    bishop = R.drawable.bishop_white,
+    knight = R.drawable.knight_white,
+    queen = R.drawable.queen_white,
+    king = R.drawable.king_white,
+)
+
+val DefaultBlackPieces = Pieces(
+    pawn = R.drawable.pawn_black,
+    rook = R.drawable.rook_black,
+    bishop = R.drawable.bishop_black,
+    knight = R.drawable.knight_black,
+    queen = R.drawable.queen_black,
+    king = R.drawable.king_black,
+)
+
+val DefaultPieceSet = PieceSet(white = DefaultWhitePieces, black = DefaultBlackPieces)
+
+val LocalPieceSet = staticCompositionLocalOf { PieceSet() }
+
+val PieceIcons: PieceSet
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalPieceSet.current
