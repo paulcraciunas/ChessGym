b2989327a79e9c60ff07626f381640da87f704fc
Paul.Craciunas <paul.craciunas@team.bumble.com>
2024-12-22 20:03:12 +0000

    Created composable for entire chess Board

---
+++ app/src/main/java/com/paulcraciunas/chessgym/ui/model/BoardViewData.kt
@@ -0,0 +1,36 @@
+package com.paulcraciunas.chessgym.ui.model
+
+import com.paulcraciunas.game.Side
+import com.paulcraciunas.game.board.File
+import com.paulcraciunas.game.board.Piece
+import com.paulcraciunas.game.board.Rank
+
+data class BoardViewData(
+    private val squares: Array<Array<SquareViewData>>,
+) {
+    fun at(rank: Rank, file: File): SquareViewData = squares[rank.dec()][file.dec()]
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BoardViewData
+
+        return squares.contentDeepEquals(other.squares)
+    }
+
+    override fun hashCode(): Int = squares.contentDeepHashCode()
+}
+
+data class SquareViewData(
+    val piece: PieceViewData?,
+    val canMoveTo: Boolean = false,
+    val lastMove: Boolean = false,
+)
+
+data class PieceViewData(
+    val piece: Piece,
+    val side: Side,
+    val isSelected: Boolean = false
+)
+
