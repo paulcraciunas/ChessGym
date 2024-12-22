cdda8550c1b5e174838155f1e9f93fb0b90cfc84
Paul.Craciunas <paul.craciunas@team.bumble.com>
2024-12-22 17:02:27 +0000

    Created composable for Chess Piece

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":game"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
