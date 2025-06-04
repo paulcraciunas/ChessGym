de9f303e9f5df61ac920c03e4e1a881149069bc8
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-06-04 10:55:06 +0100

    Added missing permissions and fixed gradle warning

--- game/data/build.gradle.kts
+++ game/data/build.gradle.kts
@@ -15,9 +15,9 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
-        ksp {
-            arg("room.schemaLocation", "$projectDir/schemas")
-        }
+    }
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
     }
 
     buildTypes {
