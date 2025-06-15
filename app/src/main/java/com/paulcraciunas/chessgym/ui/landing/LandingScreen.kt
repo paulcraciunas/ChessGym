abdb4ab1ce9a67b906a8683d4061372034685673
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-06-15 14:28:10 +0100

    Removed references in the UI as this has to be rewritten. I'll do it in a separate task

--- app/src/main/java/com/paulcraciunas/chessgym/ui/landing/LandingScreen.kt
+++ app/src/main/java/com/paulcraciunas/chessgym/ui/landing/LandingScreen.kt
@@ -8,22 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.work.WorkInfo
-import com.paulcraciunas.chessgym.R
-import com.paulcraciunas.puzzles.impl.network.PuzzleSyncWorker
 import com.paulcraciunas.settings.user.UserSettings
 
 //TODO Paul: This is only temporary. Delete this and reimplement it properly
@@ -33,66 +22,66 @@ fun LandingScreen(
     viewModel: LandingViewModel,
     modifier: Modifier = Modifier
 ) {
-    val workInfo = viewModel.workInfoLiveData.observeAsState().value
-    val isDownloading = workInfo?.state == WorkInfo.State.RUNNING
-    val isDownloaded = settings.puzzlesDownloaded || workInfo?.state == WorkInfo.State.SUCCEEDED
-    val progress by viewModel.progress.collectAsState()
-    val step by viewModel.step.collectAsState()
-    val context = LocalContext.current
-
-    if (!isDownloaded) {
-        DownloadPromptScreen(
-            isDownloading = isDownloading,
-            onStartDownload = { viewModel.startPuzzleDownload(context) },
-            progress,
-            step,
-            modifier
-        )
-    } else {
-        UserStatsScreen(settings, modifier)
-    }
-}
-
-@Composable
-fun DownloadPromptScreen(
-    isDownloading: Boolean,
-    onStartDownload: () -> Unit,
-    progress: Int,
-    step: PuzzleSyncWorker.Step, // TODO Paul: this should be abstracted away
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        if (isDownloading) {
-            CircularProgressIndicator(
-                progress = { progress / 100f },
-                trackColor = ProgressIndicatorDefaults.circularDeterminateTrackColor,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = stringResource(
-                    when (step) {
-                        PuzzleSyncWorker.Step.Download -> R.string.download_progress
-                        PuzzleSyncWorker.Step.Unpack -> R.string.unpack_progress
-                        PuzzleSyncWorker.Step.BuildDb -> R.string.write_to_db_progress
-                    },
-                    progress
-                )
-            )
-        } else {
-            Text(text = stringResource(id = R.string.download_prompt))
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(onClick = onStartDownload) {
-                Text(text = stringResource(id = R.string.download_button))
-            }
-        }
-    }
+//    val workInfo = viewModel.workInfoLiveData.observeAsState().value
+//    val isDownloading = workInfo?.state == WorkInfo.State.RUNNING
+//    val isDownloaded = settings.puzzlesDownloaded || workInfo?.state == WorkInfo.State.SUCCEEDED
+//    val progress by viewModel.progress.collectAsState()
+//    val step by viewModel.step.collectAsState()
+//    val context = LocalContext.current
+//
+//    if (!isDownloaded) {
+//        DownloadPromptScreen(
+//            isDownloading = isDownloading,
+//            onStartDownload = { viewModel.startPuzzleDownload(context) },
+//            progress,
+//            step,
+//            modifier
+//        )
+//    } else {
+//        UserStatsScreen(settings, modifier)
+//    }
+//}
+//
+//@Composable
+//fun DownloadPromptScreen(
+//    isDownloading: Boolean,
+//    onStartDownload: () -> Unit,
+//    progress: Int,
+//    step: PuzzleSyncWorker.Step, // TODO Paul: this should be abstracted away
+//    modifier: Modifier = Modifier
+//) {
+//    Column(
+//        modifier = modifier
+//            .fillMaxSize()
+//            .padding(24.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+//    ) {
+//        if (isDownloading) {
+//            CircularProgressIndicator(
+//                progress = { progress / 100f },
+//                trackColor = ProgressIndicatorDefaults.circularDeterminateTrackColor,
+//            )
+//            Spacer(modifier = Modifier.height(8.dp))
+//            Text(
+//                text = stringResource(
+//                    when (step) {
+//                        PuzzleSyncWorker.Step.Download -> R.string.download_progress
+//                        PuzzleSyncWorker.Step.Unpack -> R.string.unpack_progress
+//                        PuzzleSyncWorker.Step.BuildDb -> R.string.write_to_db_progress
+//                    },
+//                    progress
+//                )
+//            )
+//        } else {
+//            Text(text = stringResource(id = R.string.download_prompt))
+//            Spacer(modifier = Modifier.height(16.dp))
+//
+//            Button(onClick = onStartDownload) {
+//                Text(text = stringResource(id = R.string.download_button))
+//            }
+//        }
+//    }
 }
 
 @Composable
@@ -122,25 +111,25 @@ fun UserStatsScreen(
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun DownloadPromptScreenPreview_Idle() {
-    DownloadPromptScreen(
-        isDownloading = false,
-        onStartDownload = {},
-        progress = 0,
-        step = PuzzleSyncWorker.Step.Download
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DownloadPromptScreenPreview_Downloading() {
-    DownloadPromptScreen(
-        isDownloading = true,
-        onStartDownload = {},
-        progress = 45,
-        step = PuzzleSyncWorker.Step.BuildDb
-    )
-}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DownloadPromptScreenPreview_Idle() {
+//    DownloadPromptScreen(
+//        isDownloading = false,
+//        onStartDownload = {},
+//        progress = 0,
+//        step = PuzzleSyncWorker.Step.Download
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DownloadPromptScreenPreview_Downloading() {
+//    DownloadPromptScreen(
+//        isDownloading = true,
+//        onStartDownload = {},
+//        progress = 45,
+//        step = PuzzleSyncWorker.Step.BuildDb
+//    )
+//}
