9aed68a89fa0e5a2788eebb30538f47727d0d786
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-06-15 12:19:08 +0100

    Refactored the PuzzleSyncWorker to use the new classes that each have a single clear responsibility

--- game/puzzles/impl/src/main/java/com/paulcraciunas/puzzles/impl/network/PuzzleSyncWorker.kt
+++ game/puzzles/impl/src/main/java/com/paulcraciunas/puzzles/impl/network/PuzzleSyncWorker.kt
@@ -9,49 +9,48 @@ import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
-import com.github.luben.zstd.ZstdInputStream
 import com.paulcraciunas.notifications.api.NotificationFactory
-import com.paulcraciunas.puzzles.impl.db.Puzzle
-import com.paulcraciunas.puzzles.impl.impl.PuzzleDatabase
-import com.paulcraciunas.serializer.api.PuzzleWriter
+import com.paulcraciunas.puzzles.impl.network.progress.ProgressReporter
+import com.paulcraciunas.puzzles.impl.network.save.PuzzleDatabaseWriter
+import com.paulcraciunas.puzzles.impl.network.source.PuzzleDatabaseSource
+import com.paulcraciunas.puzzles.impl.network.unpack.FileDecompressor
+import com.paulcraciunas.puzzles.impl.network.writer.FileWriter
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.net.HttpURLConnection
-import java.net.URL
 
 @HiltWorker
 class PuzzleSyncWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParams: WorkerParameters,
-    private val puzzleWriter: PuzzleWriter,
     private val notificationFactory: NotificationFactory,
-    private val progressReporter: WorkerProgressReporter,
-    private val db: PuzzleDatabase
+    private val progressReporter: ProgressReporter,
+    private val databaseSource: PuzzleDatabaseSource,
+    private val fileWriter: FileWriter,
+    private val decompressor: FileDecompressor,
+    private val puzzleDatabaseWriter: PuzzleDatabaseWriter,
 ) : CoroutineWorker(context, workerParams) {
 
-    private val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
-    private var bytes: Int = 0
     private var step: Step = Step.Download
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         setForegroundAsync(createForegroundInfo())
-        progressReporter.init { progress -> setProgress(workDataOf(STEP to step.toString(), PROGRESS_NAME to progress)) }
+        progressReporter.init { progress ->
+            setProgress(workDataOf(STEP to step.toString(), PROGRESS_NAME to progress))
+        }
 
         val zstFile = File(applicationContext.cacheDir, "puzzles.zst")
         val csvFile = File(applicationContext.cacheDir, "puzzles.csv")
 
         try {
-            downloadDb(URL(LICHESS_URL), zstFile)
-            decompressZst(zstFile, csvFile)
-            writeDb(csvFile)
+            downloadPuzzleDatabase(zstFile)
+            decompressPuzzleDatabase(zstFile, csvFile)
+            writePuzzlesToDatabase(csvFile)
             return@withContext Result.success()
         } catch (e: Exception) {
-            e.printStackTrace()
+            Log.e(PuzzleSyncWorker::class.java.canonicalName, "Failed to provision puzzle database. Will retry later", e)
             return@withContext Result.retry()
         } finally {
             zstFile.delete()
@@ -59,8 +58,25 @@ class PuzzleSyncWorker @AssistedInject constructor(
         }
     }
 
+    private suspend fun downloadPuzzleDatabase(destination: File) {
+        step = Step.Download
+        fileWriter.onBegin(databaseSource.open())
+        fileWriter.write(databaseSource.read(), destination)
+    }
+
+    private suspend fun decompressPuzzleDatabase(source: File, destination: File) {
+        step = Step.Unpack
+        fileWriter.onBegin((source.length() * COMPRESS_FACTOR).toLong())
+        fileWriter.write(decompressor.decompress(source.inputStream()), destination)
+    }
+
+    private suspend fun writePuzzlesToDatabase(source: File) {
+        step = Step.BuildDb
+        progressReporter.onBegin(DB_SIZE)
+        puzzleDatabaseWriter.writePuzzlesToDatabase(source)
+    }
+
     private fun createForegroundInfo(): ForegroundInfo {
-        // Create a Notification channel if necessary
         notificationFactory.createChannel(applicationContext)
         val notification = notificationFactory.createForegroundNotification(applicationContext)
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -70,79 +86,6 @@ class PuzzleSyncWorker @AssistedInject constructor(
         }
     }
 
-    private suspend fun downloadDb(url: URL, zstFile: File) {
-        val connection = withContext(Dispatchers.IO) {
-            url.openConnection()
-        } as HttpURLConnection
-        progressReporter.onBegin(connection.contentLength.toLong())
-
-        connection.inputStream.use { input ->
-            zstFile.outputStream().use { output ->
-                bytes = input.read(buffer)
-                while (bytes >= 0) {
-                    output.write(buffer, 0, bytes)
-                    progressReporter.onCompleted(bytes)
-                    bytes = input.read(buffer)
-                }
-            }
-        }
-    }
-
-    private suspend fun decompressZst(zstFile: File, csvFile: File) {
-        step = Step.Unpack
-        progressReporter.onBegin((zstFile.length() * COMPRESS_FACTOR).toLong())
-        withContext(Dispatchers.IO) {
-            FileInputStream(zstFile).use { fis ->
-                ZstdInputStream(fis).use { zis ->
-                    FileOutputStream(csvFile).use { fos ->
-                        bytes = zis.read(buffer)
-                        while (bytes >= 0) {
-                            fos.write(buffer, 0, bytes)
-                            progressReporter.onCompleted(bytes)
-                            bytes = zis.read(buffer)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Lichess format is: PuzzleId, FEN, Moves, Rating, RatingDeviation, Popularity, NbPlays,
-     * Themes, GameUrl, OpeningTags
-     *
-     * We want to keep only the following: FEN, Moves, Rating and Themes
-     */
-    private suspend fun writeDb(csvFile: File) {
-        step = Step.BuildDb
-        progressReporter.onBegin(DB_SIZE)
-
-        val puzzles = ArrayList<Puzzle>(BULK_INSERT_COUNT)
-        csvFile.reader(Charsets.UTF_8).buffered(BUFFER_SIZE).use { reader ->
-            val lines = reader.lineSequence().drop(1)
-            lines.forEach { line ->
-                val tokens = line.split(',')
-                if (!line.startsWith("#") && tokens.size == 10) {
-                    puzzles.add(Puzzle(fenBinary = puzzleWriter.write(tokens[1], tokens[2]), rating = tokens[3].toInt()))
-                }
-                insert(puzzles)
-            }
-        }
-    }
-
-    private suspend fun insert(puzzles: ArrayList<Puzzle>) {
-        if (puzzles.size == BULK_INSERT_COUNT) {
-            try {
-                db.bulkInsert(puzzles)
-                progressReporter.onCompleted(BULK_INSERT_COUNT)
-                puzzles.clear()
-            } catch (e: Exception) {
-                Log.e(PuzzleSyncWorker::class.java.simpleName, "Bulk insert failed!", e)
-            }
-        }
-    }
-
-    // TODO Paul: move these to Repository layer
     enum class Step {
         Download,
         Unpack,
@@ -150,12 +93,8 @@ class PuzzleSyncWorker @AssistedInject constructor(
     }
 
     companion object {
-        private const val LICHESS_URL = "https://database.lichess.org/lichess_db_puzzle.csv.zst"
-        private const val COMPRESS_FACTOR = 3.7f // It's an approximation. Can't get the proper full size from the Zstd library
-        private const val BUFFER_SIZE = 32 * 1024
-        private const val DB_SIZE = 5_000_000L // The 05/2025 version has 4,824,507 puzzles. We'll use this as an approximation for progress
-        private const val BULK_INSERT_COUNT = 50_000 // Insert 50k puzzles at once, to improve performance
-
+        private const val COMPRESS_FACTOR = 3.7f
+        private const val DB_SIZE = 5_000_000L
         const val STEP = "step"
         const val PROGRESS_NAME = "progress"
         const val TAG = "PuzzleSync"
