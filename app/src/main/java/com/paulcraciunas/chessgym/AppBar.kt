ef35cf9dc79f0bd046599d4c82c00bce7b470bbe
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-06-22 22:51:33 +0100

    Made the AppBar configurable to be also back

--- app/src/main/java/com/paulcraciunas/chessgym/AppBar.kt
+++ app/src/main/java/com/paulcraciunas/chessgym/AppBar.kt
@@ -2,6 +2,7 @@ package com.paulcraciunas.chessgym
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -11,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,36 +22,68 @@ import com.paulcraciunas.chessgym.ui.theme.ChessGymTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBar(
-    onHome: () -> Unit,
-    modifier: Modifier = Modifier
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    title: String = stringResource(R.string.app_name),
+    homeButton: (@Composable AppBarScope.() -> Unit)
 ) {
+    val appBarScope = remember { AppBarScope() }
+
     TopAppBar(
         colors = topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             titleContentColor = MaterialTheme.colorScheme.primary,
         ),
-        title = {
-            Text(text = stringResource(R.string.app_name))
-        },
+        title = { Text(text = title) },
         navigationIcon = {
-            IconButton(onClick = { onHome() }) {
-                Icon(
-                    imageVector = Icons.Default.Menu,
-                    contentDescription = stringResource(R.string.nav_drawer_menu)
-                )
+            IconButton(onClick = { onClick() }) {
+                homeButton(appBarScope)
             }
         },
         modifier = modifier
     )
 }
 
-@Preview("Drawer contents")
-@Preview("Drawer contents (dark)", uiMode = UI_MODE_NIGHT_YES)
+@Stable
+class AppBarScope internal constructor() {
+    @Composable
+    fun Home(modifier: Modifier = Modifier) {
+        Icon(
+            modifier = modifier,
+            imageVector = Icons.Default.Menu,
+            contentDescription = stringResource(R.string.nav_drawer_menu)
+        )
+    }
+
+    @Composable
+    fun Back(modifier: Modifier = Modifier) {
+        Icon(
+            modifier = modifier,
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = stringResource(R.string.nav_drawer_back)
+        )
+    }
+}
+
+@Preview("Home AppBar")
+@Preview("Home AppBar (dark)", uiMode = UI_MODE_NIGHT_YES)
 @Composable
-fun PreviewAppBar() {
+fun PreviewAppBar_Home() {
     ChessGymTheme {
         AppBar(
-            onHome = {},
-        )
+            onClick = {},
+        ) { Home() }
+    }
+}
+
+@Preview("Back AppBar")
+@Preview("Back AppBar (dark)", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewAppBar_Back() {
+    ChessGymTheme {
+        AppBar(
+            onClick = {},
+            title = "Back"
+        ) { Back() }
     }
 }
