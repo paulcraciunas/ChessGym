29d0e9e416525f29d929cda291c9759f50b8e04b
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-07-04 18:58:23 +0100

    Fixed a couple of issues with previews and theme colors being overridden

--- screens/common/src/main/java/com/paulcraciunas/screens/common/theme/Theme.kt
+++ screens/common/src/main/java/com/paulcraciunas/screens/common/theme/Theme.kt
@@ -33,7 +33,7 @@ fun ChessGymTheme(
     boardTheme: BoardTheme = BoardTheme.Wood,
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
