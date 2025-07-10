796778aedde77bd65c90dde5632eaf2416aa5d36
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-07-10 20:53:35 +0100

    Removed unused imports

--- screens/puzzles/rated/ui/src/main/java/com/paulcraciunas/screens/puzzles/rated/ui/PuzzleControls.kt
+++ screens/puzzles/rated/ui/src/main/java/com/paulcraciunas/screens/puzzles/rated/ui/PuzzleControls.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
