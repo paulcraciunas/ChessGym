c171aea776b0fddbca7e62603ddddcf51d23754f
Paul.Craciunas <paul.craciunas@team.bumble.com>
2025-07-31 16:03:19 +0100

    Updated the domain module injections

--- domain/di/src/main/java/com/paulcraciunas/domain/di/DomainModule.kt
+++ domain/di/src/main/java/com/paulcraciunas/domain/di/DomainModule.kt
@@ -2,10 +2,14 @@ package com.paulcraciunas.domain.di
 
 import com.paulcraciunas.domain.api.CalculateElo
 import com.paulcraciunas.domain.api.GetPuzzleByRating
+import com.paulcraciunas.domain.api.GetPuzzleSeries
 import com.paulcraciunas.domain.api.OnPuzzleComplete
+import com.paulcraciunas.domain.api.RandomFactory
 import com.paulcraciunas.domain.impl.CalculateEloImpl
 import com.paulcraciunas.domain.impl.GetPuzzleByRatingImpl
+import com.paulcraciunas.domain.impl.GetPuzzleSeriesImpl
 import com.paulcraciunas.domain.impl.OnPuzzleCompleteImpl
+import com.paulcraciunas.domain.impl.TLRandomFactory
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -19,6 +23,14 @@ internal abstract class DomainModule {
     @Binds
     @Singleton
     abstract fun bindCalculateEloUseCase(impl: CalculateEloImpl): CalculateElo
+
+    @Binds
+    @Singleton
+    abstract fun bindRandomFactory(impl: TLRandomFactory): RandomFactory
+
+    @Binds
+    @Singleton
+    abstract fun bindGetPuzzleSeries(impl: GetPuzzleSeriesImpl): GetPuzzleSeries
 
     @Binds
     @Singleton
