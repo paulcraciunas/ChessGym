a36760036414316a439c6c9d56b222b1ae44b0d2
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-01-19 15:49:34 +0800

    Removed unused imports

--- screens/common/src/main/java/com/paulcraciunas/screens/common/controls/RatingChangeChip.kt
+++ screens/common/src/main/java/com/paulcraciunas/screens/common/controls/RatingChangeChip.kt
@@ -15,10 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.paulcraciunas.screens.common.theme.LoadingColors
 import com.paulcraciunas.screens.common.theme.LoadingTheme
 
 @Composable
