431af721e9ecdffb691387ceabeb528e1c937ea7
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-01-20 17:44:05 +0800

    Added new gradle modules for the future failed puzzles ui and vm implementations

---
+++ screens/puzzles/failed/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("conventions.android.library")
+}
+
+androidLibrary {
+    namespace = "com.paulcraciunas.screens.puzzles.failed.ui"
+    consumerProguardFile("consumer-rules.pro")
+    proguardFile("proguard-rules.pro")
+    compose = true
+}
+
+dependencies {
+    implementation(project(":global:resources"))
+    implementation(project(":screens:common"))
+    implementation(project(":game:logic:api"))
+    api(project(":screens:puzzles:failed:vm"))
+}
---
+++ screens/puzzles/failed/ui/consumer-rules.pro
@@ -0,0 +1,1 @@
+# Consumer rules for failed puzzles UI module
