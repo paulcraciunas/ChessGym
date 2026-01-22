f0b4962e15c612cffeab7745c900d93bb4f4ba57
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-01-22 14:12:20 +0800

    Updated the rated puzzle screen to show loading and failed contents inside the scaffold, and the view model to use the helper

--- screens/puzzles/rated/ui/src/main/java/com/paulcraciunas/screens/puzzles/rated/ui/RatedPuzzleScreen.kt
+++ screens/puzzles/rated/ui/src/main/java/com/paulcraciunas/screens/puzzles/rated/ui/RatedPuzzleScreen.kt
@@ -20,9 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.paulcraciunas.game.logic.api.Side
-import com.paulcraciunas.game.logic.api.board.File
 import com.paulcraciunas.game.logic.api.board.Piece
-import com.paulcraciunas.game.logic.api.board.Rank
 import com.paulcraciunas.global.resources.R
 import com.paulcraciunas.screens.common.AppBar
 import com.paulcraciunas.screens.common.FailedContent
@@ -32,8 +30,8 @@ import com.paulcraciunas.screens.common.board.ChessBoard
 import com.paulcraciunas.screens.common.controls.CapturedPieces
 import com.paulcraciunas.screens.common.controls.DefaultPuzzleControls
 import com.paulcraciunas.screens.common.dialogs.PromotionDialog
-import com.paulcraciunas.screens.common.model.BoardViewData
-import com.paulcraciunas.screens.common.model.SquareViewData
+import com.paulcraciunas.screens.common.model.PuzzleData
+import com.paulcraciunas.screens.common.previews.SampleBoardViewData
 import com.paulcraciunas.screens.common.theme.ChessGymTheme
 import com.paulcraciunas.screens.puzzles.rated.vm.RatedPuzzleScreenInteractor
 import com.paulcraciunas.screens.puzzles.rated.vm.RatedPuzzleUiState
@@ -47,89 +45,108 @@ fun RatedPuzzleScreen(
     onNavigateBack: () -> Unit = {},
     interactions: RatedPuzzleScreenInteractor = StubRatedPuzzleScreenInteractor(),
 ) {
-    if (uiState is RatedPuzzleUiState.Loading) {
-        LoadingContent(modifier = Modifier.fillMaxSize())
-        return
+    val title = when (uiState) {
+        is RatedPuzzleUiState.BoardState -> stringResource(R.string.rated_puzzle_title, uiState.data.rating)
+        else -> stringResource(R.string.puzzle_mode_rated_title)
     }
-    if (uiState is RatedPuzzleUiState.Failed) {
-        FailedContent(modifier = Modifier.fillMaxSize())
-        return
-    }
-    val data = (uiState as RatedPuzzleUiState.BoardState).data
     Scaffold(
         topBar = {
             AppBar(
-                title = stringResource(R.string.rated_puzzle_title, data.rating),
+                title = title,
                 navButton = { Back(onClick = onNavigateBack) }
             )
         },
         modifier = modifier
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            CapturedPieces(
-                capturedPieces = data.captured[data.player.other()]!!,
-                side = data.player,
-                modifier = Modifier.fillMaxWidth()
-            )
-            ChessBoard(
-                board = data.boardData,
-                orientation = BoardOrientation.fromSide(data.player),
-                onClick = interactions::onSquareClicked,
-                showBorders = showBorders,
-                modifier = Modifier.fillMaxWidth()
-            )
-            CapturedPieces(
-                capturedPieces = data.captured[data.player]!!,
-                side = data.player.other(),
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            AnimatedContent(
-                targetState = uiState is RatedPuzzleUiState.Finished,
-                transitionSpec = {
-                    (slideInVertically { height -> height } + fadeIn())
-                        .togetherWith(slideOutVertically { height -> -height } + fadeOut())
-                },
-                label = "ControlsAnimation"
-            ) { isFinished ->
-                if (isFinished && uiState is RatedPuzzleUiState.Finished) {
-                    FinishedPuzzleControls(
-                        success = uiState.success,
-                        ratingChange = uiState.ratingChange,
-                        modifier = Modifier.fillMaxWidth(),
-                        onPlayNext = interactions::onNextPuzzle,
-                    )
-                } else if (uiState is RatedPuzzleUiState.Playing) {
-                    DefaultPuzzleControls(
-                        hintEnabled = uiState.hintEnabled,
-                        toMove = data.player,
-                        onHintRequested = interactions::onHintRequested,
-                        onAbandonRequested = interactions::onAbandon,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
+        when (uiState) {
+            is RatedPuzzleUiState.Loading -> {
+                LoadingContent(modifier = Modifier.fillMaxSize().padding(innerPadding))
             }
-            // Dialogs for playing state
-            if (uiState is RatedPuzzleUiState.Playing) {
-                if (uiState.showAbandonDialog) {
-                    AbandonConfirmationDialog(
-                        onConfirm = interactions::onAbandonConfirmed,
-                        onDismiss = interactions::onAbandonDismissed
-                    )
-                }
-                if (uiState.promotion != null) {
-                    PromotionDialog(
-                        side = data.player,
-                        onPieceChosen = interactions::onPromote
-                    )
-                }
+            is RatedPuzzleUiState.Failed -> {
+                FailedContent(modifier = Modifier.fillMaxSize().padding(innerPadding))
             }
-            Spacer(modifier = Modifier.height(8.dp))
+            is RatedPuzzleUiState.BoardState -> {
+                RatedPuzzleContent(
+                    uiState = uiState,
+                    showBorders = showBorders,
+                    interactions = interactions,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
+    }
+}
+
+@Composable
+private fun RatedPuzzleContent(
+    uiState: RatedPuzzleUiState.BoardState,
+    showBorders: Boolean,
+    interactions: RatedPuzzleScreenInteractor,
+    modifier: Modifier = Modifier,
+) {
+    val data = uiState.data
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        CapturedPieces(
+            capturedPieces = data.captured[data.player.other()]!!,
+            side = data.player,
+            modifier = Modifier.fillMaxWidth()
+        )
+        ChessBoard(
+            board = data.boardData,
+            orientation = BoardOrientation.fromSide(data.player),
+            onClick = interactions::onSquareClicked,
+            showBorders = showBorders,
+            modifier = Modifier.fillMaxWidth()
+        )
+        CapturedPieces(
+            capturedPieces = data.captured[data.player]!!,
+            side = data.player.other(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        AnimatedContent(
+            targetState = uiState is RatedPuzzleUiState.Finished,
+            transitionSpec = {
+                (slideInVertically { height -> height } + fadeIn())
+                    .togetherWith(slideOutVertically { height -> -height } + fadeOut())
+            },
+            label = "ControlsAnimation"
+        ) { isFinished ->
+            if (isFinished && uiState is RatedPuzzleUiState.Finished) {
+                FinishedPuzzleControls(
+                    success = uiState.success,
+                    ratingChange = uiState.ratingChange,
+                    modifier = Modifier.fillMaxWidth(),
+                    onPlayNext = interactions::onNextPuzzle,
+                )
+            } else if (uiState is RatedPuzzleUiState.Playing) {
+                DefaultPuzzleControls(
+                    hintEnabled = uiState.hintEnabled,
+                    toMove = data.player,
+                    onHintRequested = interactions::onHintRequested,
+                    onAbandonRequested = interactions::onAbandon,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+        // Dialogs for playing state
+        if (uiState is RatedPuzzleUiState.Playing) {
+            if (uiState.showAbandonDialog) {
+                AbandonConfirmationDialog(
+                    onConfirm = interactions::onAbandonConfirmed,
+                    onDismiss = interactions::onAbandonDismissed
+                )
+            }
+            if (uiState.promotion != null) {
+                PromotionDialog(
+                    side = data.player,
+                    onPieceChosen = interactions::onPromote
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
 
@@ -140,10 +157,10 @@ private fun WhitePlayingPreview() {
     ChessGymTheme {
         RatedPuzzleScreen(
             uiState = RatedPuzzleUiState.Playing(
-                data = RatedPuzzleUiState.PuzzleData(
+                data = PuzzleData(
                     rating = 1450,
                     player = Side.WHITE,
-                    boardData = sampleBoard(),
+                    boardData = SampleBoardViewData.defaultBoard(),
                     captured = hashMapOf(
                         Side.WHITE to listOf(Piece.Pawn, Piece.Knight, Piece.Pawn),
                         Side.BLACK to listOf(Piece.Bishop, Piece.Pawn, Piece.Pawn, Piece.Rook)
@@ -165,10 +182,10 @@ private fun BlackPlayingPreview() {
     ChessGymTheme {
         RatedPuzzleScreen(
             uiState = RatedPuzzleUiState.Playing(
-                data = RatedPuzzleUiState.PuzzleData(
+                data = PuzzleData(
                     rating = 1450,
                     player = Side.BLACK,
-                    boardData = sampleBoard(),
+                    boardData = SampleBoardViewData.defaultBoard(),
                     captured = hashMapOf(
                         Side.WHITE to listOf(Piece.Pawn, Piece.Knight, Piece.Pawn),
                         Side.BLACK to listOf(Piece.Bishop, Piece.Pawn, Piece.Pawn, Piece.Rook)
@@ -181,40 +198,4 @@ private fun BlackPlayingPreview() {
             showBorders = true
         )
     }
-}
-
-private fun sampleBoard(): BoardViewData {
-    val squares: Array<Array<SquareViewData>> = Array(Rank.entries.size) {
-        Array(File.entries.size) { SquareViewData(piece = null) }
-    }
-    squares.addWhitePieces()
-    squares.addBlackPieces()
-
-    return BoardViewData(squares)
-}
-
-
-private fun Array<Array<SquareViewData>>.addWhitePieces() = apply {
-    File.entries.forEach { file ->
-        this[Rank.`2`.dec()][file.dec()] = SquareViewData.simple(piece = Piece.Pawn, side = Side.WHITE)
-    }
-    addStartingPieces(Side.WHITE, Rank.`1`)
-}
-
-private fun Array<Array<SquareViewData>>.addBlackPieces() = apply {
-    File.entries.forEach { file ->
-        this[Rank.`7`.dec()][file.dec()] = SquareViewData.simple(piece = Piece.Pawn, side = Side.BLACK)
-    }
-    addStartingPieces(Side.BLACK, Rank.`8`)
-}
-
-private fun Array<Array<SquareViewData>>.addStartingPieces(side: Side, rank: Rank) {
-    this[rank.dec()][File.a.dec()] = SquareViewData.simple(piece = Piece.Rook, side = side)
-    this[rank.dec()][File.b.dec()] = SquareViewData.simple(piece = Piece.Knight, side = side)
-    this[rank.dec()][File.c.dec()] = SquareViewData.simple(piece = Piece.Bishop, side = side)
-    this[rank.dec()][File.d.dec()] = SquareViewData.simple(piece = Piece.Queen, side = side)
-    this[rank.dec()][File.e.dec()] = SquareViewData.simple(piece = Piece.King, side = side)
-    this[rank.dec()][File.f.dec()] = SquareViewData.simple(piece = Piece.Bishop, side = side)
-    this[rank.dec()][File.g.dec()] = SquareViewData.simple(piece = Piece.Knight, side = side)
-    this[rank.dec()][File.h.dec()] = SquareViewData.simple(piece = Piece.Rook, side = side)
 }
