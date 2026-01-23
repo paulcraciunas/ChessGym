09f3fc6eb25a4e46476c775d143a5741004a9096
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-01-23 13:32:25 +0800

    Added new gradle modules for the Board Visualisation dashboard and find the square game

---
+++ screens/boardvis/squares/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("conventions.android.library")
+}
+
+androidLibrary {
+    namespace = "com.paulcraciunas.screens.boardvis.squares.ui"
+    consumerProguardFile("consumer-rules.pro")
+    proguardFile("proguard-rules.pro")
+    compose = true
+}
+
+dependencies {
+    implementation(project(":global:resources"))
+    implementation(project(":screens:common"))
+    implementation(project(":settings:application:api"))
+    api(project(":screens:boardvis:squares:vm"))
+}
