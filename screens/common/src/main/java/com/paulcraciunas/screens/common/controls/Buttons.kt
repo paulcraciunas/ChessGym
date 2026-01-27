6d667f0181df059818643cc515d4dfe5fd9981e6
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-01-27 19:54:41 +0500

    Added preview for the common Button composables

--- screens/common/src/main/java/com/paulcraciunas/screens/common/controls/Buttons.kt
+++ screens/common/src/main/java/com/paulcraciunas/screens/common/controls/Buttons.kt
@@ -1,5 +1,6 @@
 package com.paulcraciunas.screens.common.controls
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -15,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.paulcraciunas.global.resources.R
+import com.paulcraciunas.screens.common.theme.ChessGymTheme
 
 @Composable
 fun PlayButton(
@@ -63,5 +66,23 @@ fun RefreshButton(
             style = MaterialTheme.typography.titleMedium,
             modifier = Modifier.padding(vertical = 4.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview("Dark mode", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun PlayButtonPreview() {
+    ChessGymTheme {
+        PlayButton(onClick = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Preview("Dark mode", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun RefreshButtonPreview() {
+    ChessGymTheme {
+        RefreshButton(onClick = {})
     }
 }
