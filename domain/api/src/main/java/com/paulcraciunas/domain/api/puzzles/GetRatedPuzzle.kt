e8191bfc54e6f48d6d2f8a6159509805e00e1ae6
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-02-17 13:32:18 +0000

    Updated the GetRatedPuzzle use case to produce a random puzzle within a rating deviation, instead of a fixed rating puzzle

--- domain/api/src/main/java/com/paulcraciunas/domain/api/puzzles/GetRatedPuzzle.kt
+++ domain/api/src/main/java/com/paulcraciunas/domain/api/puzzles/GetRatedPuzzle.kt
@@ -4,10 +4,17 @@ import com.paulcraciunas.domain.api.general.EloResult
 import com.paulcraciunas.game.logic.api.Puzzle
 
 interface GetRatedPuzzle {
+    /**
+     * By default, will get a puzzle +/- [DEVIATION] rating points away from the user's rating.
+     **/
     suspend operator fun invoke(): Data
 
     class Data(
         val puzzle: Puzzle,
         val ratingChange: EloResult,
     )
+
+    companion object {
+        const val DEVIATION = 25
+    }
 }
