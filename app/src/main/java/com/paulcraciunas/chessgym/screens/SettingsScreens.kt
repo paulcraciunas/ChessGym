733b0d16f386fb955c55cb442c75afc7b8fa1371
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-02-23 23:14:15 +0000

    Implemented the Application Settings UI

---
+++ app/src/main/java/com/paulcraciunas/chessgym/screens/SettingsScreens.kt
@@ -0,0 +1,26 @@
+package com.paulcraciunas.chessgym.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.paulcraciunas.chessgym.BuildConfig
+import com.paulcraciunas.screens.settings.ui.SettingsScreen
+import com.paulcraciunas.screens.settings.vm.SettingsViewModel
+
+@Composable
+internal fun Settings(
+    onNavigateBack: () -> Unit,
+) {
+    val vm: SettingsViewModel = hiltViewModel()
+    val settingsState by vm.uiState.collectAsStateWithLifecycle()
+
+    val buildVersion = "Version ${BuildConfig.APP_VERSION}-${BuildConfig.BUILD_NUMBER}"
+
+    SettingsScreen(
+        uiState = settingsState,
+        buildVersion = buildVersion,
+        onNavigateBack = onNavigateBack,
+        interactions = vm,
+    )
+}
