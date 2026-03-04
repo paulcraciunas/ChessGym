231d491eaafe8d147d0efa74685d8a6fa00c16de
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-03-04 14:25:58 +0000

    Fixed input/output communication with the engine via standard i/o

--- game/engine/impl/src/main/cpp/bridge.cpp
+++ game/engine/impl/src/main/cpp/bridge.cpp
@@ -16,6 +16,10 @@ namespace PSQT { void init(); }
 
 static int input_pipe[2];
 static int output_pipe[2];
+
+__attribute__((unused)) static int saved_stdin  = -1;
+__attribute__((unused)) static int saved_stdout = -1;
+
 static FILE *stockfish_in = nullptr;
 static FILE *stockfish_out = nullptr;
 
@@ -26,10 +30,11 @@ static void stockfish_main() {
     Position::init();
     Bitbases::init();
     Endgames::init();
-    Threads.set(Options["Threads"]);
+    Threads.set(Options["Threads"]); // NOLINT(*-narrowing-conversions)
     Search::clear();
 
-    UCI::loop(0, nullptr);
+    // argc must be 1 so UCI::loop calls getline(cin, cmd) to read from stdin
+    UCI::loop(1, nullptr);
 
     Threads.set(0);
 }
@@ -39,11 +44,23 @@ Java_com_paulcraciunas_game_engine_impl_StockfishBridge_nativeStartEngine(JNIEnv
     pipe(input_pipe);
     pipe(output_pipe);
 
+    // Save original fds so they aren't leaked
+    saved_stdin  = dup(STDIN_FILENO);
+    saved_stdout = dup(STDOUT_FILENO);
+
+    // Redirect stdin/stdout to our pipes
     dup2(input_pipe[0], STDIN_FILENO);
     dup2(output_pipe[1], STDOUT_FILENO);
 
+    // Close the duplicated pipe ends that are now unused in this (Java) thread
+    close(input_pipe[0]);
+    close(output_pipe[1]);
+
     stockfish_in = fdopen(input_pipe[1], "w");
     stockfish_out = fdopen(output_pipe[0], "r");
+
+    // Disable buffering on the output reader so we get lines immediately
+    setvbuf(stockfish_out, nullptr, _IONBF, 0);
 
     std::thread(stockfish_main).detach();
 }
