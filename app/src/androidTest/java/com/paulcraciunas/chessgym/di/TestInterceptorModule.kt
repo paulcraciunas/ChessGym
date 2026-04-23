16804f60965ee491e3e345460cf66c284142612a
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-04-23 16:22:23 +0100

    Removed unused imports

--- app/src/androidTest/java/com/paulcraciunas/chessgym/di/TestInterceptorModule.kt
+++ app/src/androidTest/java/com/paulcraciunas/chessgym/di/TestInterceptorModule.kt
@@ -2,7 +2,6 @@ package com.paulcraciunas.chessgym.di
 
 import com.paulcraciunas.puzzles.api.PuzzleInterceptor
 import com.paulcraciunas.puzzles.di.PuzzleInterceptorModule
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
--- game/puzzles/di/src/main/java/com/paulcraciunas/puzzles/di/PuzzleInterceptorModule.kt
+++ game/puzzles/di/src/main/java/com/paulcraciunas/puzzles/di/PuzzleInterceptorModule.kt
@@ -2,13 +2,11 @@ package com.paulcraciunas.puzzles.di
 
 import com.paulcraciunas.puzzles.api.PuzzleInterceptor
 import com.paulcraciunas.puzzles.impl.impl.LoggingPuzzleInterceptor
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
