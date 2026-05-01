5067cea1398ecaa06925a33276a5a5cc1df0d065
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-01 16:29:39 +0100

    Fixed issue where puzzles loaded into analysis were started on the opponent's move

--- domain/impl/src/main/java/com/paulcraciunas/domain/impl/puzzles/GetPuzzleFenImpl.kt
+++ domain/impl/src/main/java/com/paulcraciunas/domain/impl/puzzles/GetPuzzleFenImpl.kt
@@ -11,6 +11,10 @@ class GetPuzzleFenImpl(
 
     override suspend fun invoke(puzzleId: Int): String? {
         val puzzle = puzzleRepository.getById(puzzleId) ?: return null
+        // It's important to make the opponent move before we get the position for the player's move
+        puzzle.start()
+        puzzle.playNextMove()
+        // Now we can serialize the position
         return fenSerializer.of(puzzle)
     }
 }
