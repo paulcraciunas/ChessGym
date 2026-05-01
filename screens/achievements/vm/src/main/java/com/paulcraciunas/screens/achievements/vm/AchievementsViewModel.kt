f8ece60479665ab028c04a2eef58752eeee3c15e
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-01 12:42:28 +0100

    Removed unused imports

--- screens/achievements/vm/src/main/java/com/paulcraciunas/screens/achievements/vm/AchievementsViewModel.kt
+++ screens/achievements/vm/src/main/java/com/paulcraciunas/screens/achievements/vm/AchievementsViewModel.kt
@@ -5,14 +5,12 @@ import androidx.lifecycle.viewModelScope
 import com.paulcraciunas.domain.api.achievements.GetAchievementState
 import com.paulcraciunas.domain.api.achievements.MarkAchievementsSeen
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
