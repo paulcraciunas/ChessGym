348fc2c1b1a173133f57559d798d8b8635c0d126
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-08 17:07:19 +0100

    Added ui and vm Gradle modules for the SignIn screen

---
+++ screens/signin/vm/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("conventions.android.library")
+}
+
+androidLibrary {
+    namespace = "com.paulcraciunas.screens.signin.vm"
+    consumerProguardFile("consumer-rules.pro")
+    proguardFile("proguard-rules.pro")
+    di = true
+}
+
+dependencies {
+    implementation(project(":domain:api"))
+    implementation(project(":user:api"))
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.public.timber)
+
+    testImplementation(testFixtures(project(":user:api")))
+    testImplementation(testFixtures(project(":domain:api")))
+}
