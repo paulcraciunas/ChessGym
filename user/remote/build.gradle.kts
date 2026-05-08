4f3fecb53f79c5b0a9a4fbe602c61f1eba9f51e9
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-08 17:25:03 +0100

    Added missing dependencies for the user gradle modules

--- user/remote/build.gradle.kts
+++ user/remote/build.gradle.kts
@@ -2,8 +2,17 @@ plugins {
     id("conventions.library")
 }
 
+library {
+    serialization = true
+}
+
 dependencies {
     api(project(":user:api"))
 
+    implementation(project(":global:qualifiers"))
+
     implementation(libs.javax.inject)
+    implementation(libs.bundles.ktor.client)
+
+    testImplementation(libs.ktor.client.mock)
 }
