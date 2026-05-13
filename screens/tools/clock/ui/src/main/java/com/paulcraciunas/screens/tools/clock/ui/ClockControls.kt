e7c9f293a82fe9d1bb37ed71aedc5134e4317a47
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-13 10:31:47 +0100

    Removed unused import

--- screens/tools/clock/ui/src/main/java/com/paulcraciunas/screens/tools/clock/ui/ClockControls.kt
+++ screens/tools/clock/ui/src/main/java/com/paulcraciunas/screens/tools/clock/ui/ClockControls.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.paulcraciunas.domain.api.general.CountdownTimer
 import com.paulcraciunas.game.logic.api.Side
 import com.paulcraciunas.global.resources.R
