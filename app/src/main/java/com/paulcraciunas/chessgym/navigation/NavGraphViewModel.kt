9530a7f09bdb61246da9deb237f812eaca09c1d6
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-16 14:26:49 +0100

    Changed to early subscription so we don't miss any potential notifications and cause jarring UI jumps

--- app/src/main/java/com/paulcraciunas/chessgym/navigation/NavGraphViewModel.kt
+++ app/src/main/java/com/paulcraciunas/chessgym/navigation/NavGraphViewModel.kt
@@ -32,7 +32,7 @@ class NavGraphViewModel @Inject constructor(
         }
         .stateIn(
             scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
+            started = SharingStarted.Eagerly,
             initialValue = NavGraphUiState(isLoading = true)
         )
 }
