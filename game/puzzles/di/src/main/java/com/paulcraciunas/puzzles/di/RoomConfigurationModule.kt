e57fb5e47cea944bd3b3d166520ea4f51a03f09f
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-17 10:56:57 +0100

    Updated Room injection to always work with asset databases

--- game/puzzles/di/src/main/java/com/paulcraciunas/puzzles/di/RoomConfigurationModule.kt
+++ game/puzzles/di/src/main/java/com/paulcraciunas/puzzles/di/RoomConfigurationModule.kt
@@ -1,27 +1,28 @@
 package com.paulcraciunas.puzzles.di
 
+import com.paulcraciunas.puzzles.api.PuzzleDatabaseContract
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Qualifier
 
-enum class DbVariant {
-    FileSystem,
-    Asset
-}
-
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class DbName
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DbAssetPath
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RoomConfigurationModule {
     @Provides
     @DbName
-    fun provideDbName(): String = "puzzle_database"
+    fun provideDbName(): String = PuzzleDatabaseContract.ROOM_DATABASE_NAME
 
     @Provides
-    fun provideDbVariant(): DbVariant = DbVariant.FileSystem
+    @DbAssetPath
+    fun provideDbAssetPath(): String = PuzzleDatabaseContract.LITE_ASSET_PATH
 }
