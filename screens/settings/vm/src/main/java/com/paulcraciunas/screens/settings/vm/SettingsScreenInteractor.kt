1816d6ed4c272ba724599cf981ae754469b39a6e
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-19 18:41:37 +0100

    Integrated the new application setting in the settings screen

--- screens/settings/vm/src/main/java/com/paulcraciunas/screens/settings/vm/SettingsScreenInteractor.kt
+++ screens/settings/vm/src/main/java/com/paulcraciunas/screens/settings/vm/SettingsScreenInteractor.kt
@@ -5,6 +5,7 @@ import com.paulcraciunas.settings.application.api.AppSettings
 interface SettingsScreenInteractor {
     fun onHapticFeedbackToggled(isEnabled: Boolean)
     fun onAutoPromoteToggled(isEnabled: Boolean)
+    fun onAutoNextPuzzleToggled(isEnabled: Boolean)
     fun onShowBordersToggled(isEnabled: Boolean)
     fun onHighlightLegalMovesToggled(isEnabled: Boolean)
     fun onLightModeSelected(mode: AppSettings.LightMode)
--- screens/settings/vm/src/main/java/com/paulcraciunas/screens/settings/vm/SettingsUiState.kt
+++ screens/settings/vm/src/main/java/com/paulcraciunas/screens/settings/vm/SettingsUiState.kt
@@ -5,6 +5,7 @@ import com.paulcraciunas.settings.application.api.AppSettings
 data class SettingsUiState(
     val isHapticFeedbackEnabled: Boolean = true,
     val isAutoPromoteEnabled: Boolean = true,
+    val isAutoNextPuzzleEnabled: Boolean = false,
     val isShowBordersEnabled: Boolean = true,
     val isHighlightLegalMovesEnabled: Boolean = true,
     val lightMode: AppSettings.LightMode = AppSettings.LightMode.System,
