cd3749d824f54e4dfa4f5490826cbf45b8af9961
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-22 17:02:07 +0100

    Minor improvements to backend

--- backend/src/main/kotlin/com/paulcraciunas/chessgym/routes/StatisticsRoutes.kt
+++ backend/src/main/kotlin/com/paulcraciunas/chessgym/routes/StatisticsRoutes.kt
@@ -1,15 +1,19 @@
 package com.paulcraciunas.chessgym.routes
 
+import com.paulcraciunas.chessgym.plugins.AUTH_FIREBASE
 import com.paulcraciunas.chessgym.services.StatisticsService
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.statisticsRoutes(statisticsService: StatisticsService) {
     route("/api/v1/statistics") {
-        get("/achievements") {
-            val stats = statisticsService.computeAchievementStatistics()
-            call.respond(HttpStatusCode.OK, stats)
+        authenticate(AUTH_FIREBASE) {
+            get("/achievements") {
+                val stats = statisticsService.computeAchievementStatistics()
+                call.respond(HttpStatusCode.OK, stats)
+            }
         }
     }
 }
