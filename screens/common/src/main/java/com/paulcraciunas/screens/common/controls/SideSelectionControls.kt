1c387ac4c43bc1fb4f2f30bd5d32cd02adc1bca0
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-05-29 22:49:43 +0100

    Damn unused imports again

--- screens/common/src/main/java/com/paulcraciunas/screens/common/controls/SideSelectionControls.kt
+++ screens/common/src/main/java/com/paulcraciunas/screens/common/controls/SideSelectionControls.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.paulcraciunas.game.logic.api.Side
 import com.paulcraciunas.global.resources.R
 import com.paulcraciunas.screens.common.design.theme.Design
 import com.paulcraciunas.screens.common.testTag
