cd3e45aec31d671efc866fc11aa2cc950f5a67a0
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-06-14 20:48:49 +0100

    Removed unnecessary resumable flag

--- screens/data/src/main/java/com/paulcraciunas/screens/data/engine/PlayIntent.kt
+++ screens/data/src/main/java/com/paulcraciunas/screens/data/engine/PlayIntent.kt
@@ -3,26 +3,23 @@ package com.paulcraciunas.screens.data.engine
 import com.paulcraciunas.game.logic.api.board.Locus
 import com.paulcraciunas.game.logic.api.board.Piece
 
-sealed class PlayIntent(
-    internal val blockedByAnimation: Boolean,
-    internal val resumable: Boolean,
-) {
-    data class SelectSquare(val selection: Locus) : PlayIntent(blockedByAnimation = true, resumable = false)
-    data class Promote(val to: Piece) : PlayIntent(blockedByAnimation = true, resumable = false)
-    data class Navigate(val type: Navigation) : PlayIntent(blockedByAnimation = true, resumable = false)
-    data object Hint : PlayIntent(blockedByAnimation = true, resumable = false)
-    data object RequestAbandon : PlayIntent(blockedByAnimation = true, resumable = false)
-    data object DismissAbandon : PlayIntent(blockedByAnimation = true, resumable = false)
-    data object ConfirmAbandon : PlayIntent(blockedByAnimation = true, resumable = false)
-    data object Resume : PlayIntent(blockedByAnimation = true, resumable = true)
+sealed class PlayIntent(internal val blockedByAnimation: Boolean) {
+    data class SelectSquare(val selection: Locus) : PlayIntent(blockedByAnimation = true)
+    data class Promote(val to: Piece) : PlayIntent(blockedByAnimation = true)
+    data class Navigate(val type: Navigation) : PlayIntent(blockedByAnimation = true)
+    data object Hint : PlayIntent(blockedByAnimation = true)
+    data object RequestAbandon : PlayIntent(blockedByAnimation = true)
+    data object DismissAbandon : PlayIntent(blockedByAnimation = true)
+    data object ConfirmAbandon : PlayIntent(blockedByAnimation = true)
+    data object Resume : PlayIntent(blockedByAnimation = true)
 
     internal data class AnimationPhaseComplete(
         val phase: AnimationPhase,
-    ) : PlayIntent(blockedByAnimation = false, resumable = false)
+    ) : PlayIntent(blockedByAnimation = false)
 
-    internal data object ExpireTime : PlayIntent(blockedByAnimation = false, resumable = true)
-    internal data object SessionFinished : PlayIntent(blockedByAnimation = false, resumable = false)
-    internal data object SessionFailed : PlayIntent(blockedByAnimation = false, resumable = false)
+    internal data object ExpireTime : PlayIntent(blockedByAnimation = false)
+    internal data object SessionFinished : PlayIntent(blockedByAnimation = false)
+    internal data object SessionFailed : PlayIntent(blockedByAnimation = false)
 
     enum class Navigation {
         ToStart,
