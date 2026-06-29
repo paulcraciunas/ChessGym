07242f12108d75bf2cc38fcc1e2f356a5e39227d
paulcraciunas <paulmihaicraciunas@gmail.com>
2026-06-29 19:18:56 +0100

    Clean-ed up the Application Settings setup by eliminating the need to save the app settings on cloud backup

--- app/src/main/java/com/paulcraciunas/chessgym/startup/AppSettingsProvisioningImpl.kt
+++ app/src/main/java/com/paulcraciunas/chessgym/startup/AppSettingsProvisioningImpl.kt
@@ -1,64 +1,26 @@
 package com.paulcraciunas.chessgym.startup
 
-import android.content.Context
 import com.paulcraciunas.chessgym.BuildConfig
-import com.paulcraciunas.puzzles.di.DbName
 import com.paulcraciunas.settings.application.api.AppSettings
 import com.paulcraciunas.settings.application.api.AppSettingsRepository
-import com.paulcraciunas.user.api.UserRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
-import timber.log.Timber
 import javax.inject.Inject
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class AppSettingsProvisioningImpl @Inject constructor(
     private val appSettingsRepository: AppSettingsRepository,
-    private val userRepository: UserRepository,
-    @param:ApplicationContext private val context: Context,
-    @param:DbName private val databaseName: String,
 ) : AppSettingsProvisioning {
 
     override operator fun invoke(): Flow<AppSettings> = flow {
-        try {
-            @Suppress("KotlinConstantConditions", "SimplifyBooleanWithConstants")
-            if (BuildConfig.BUILD_TYPE == "benchmark" ||
-                BuildConfig.BUILD_TYPE == "baselineProfile"
-            ) {
-                appSettingsRepository.updatePuzzlesDownloaded(true)
-                appSettingsRepository.updateMinPuzzleRating(400)
-                appSettingsRepository.updateMaxPuzzleRating(3000)
-                appSettingsRepository.updateTotalPuzzleCount(26_010)
-            } else {
-                val settings = appSettingsRepository.appSettings.first()
-                if (settings.puzzlesDownloaded) {
-                    val databaseFile = context.getDatabasePath(databaseName)
-                    if (!databaseFile.exists()) {
-                        // A device restore happened. The local DB asset was excluded,
-                        // but the old DataStore flags and user state were restored.
-                        Timber.w("Puzzle database file missing. Resetting states.")
-
-                        appSettingsRepository.updatePuzzlesDownloaded(false)
-                        val currentUser = userRepository.get()
-                        userRepository.update( // clean-up puzzle IDs as they might be stale
-                            currentUser.copy(
-                                failedPuzzles = emptyList(),
-                                ratings = currentUser.ratings.copy(
-                                    puzzleStreak = currentUser.ratings.puzzleStreak.copy(
-                                        lastPuzzleId = null,
-                                    )
-                                )
-                            )
-                        )
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            Timber.e(e, "Database integrity check failed")
+        @Suppress("KotlinConstantConditions", "SimplifyBooleanWithConstants")
+        if (BuildConfig.BUILD_TYPE == "benchmark" || BuildConfig.BUILD_TYPE == "baselineProfile") {
+            appSettingsRepository.updatePuzzlesDownloaded(true)
+            appSettingsRepository.updateMinPuzzleRating(400)
+            appSettingsRepository.updateMaxPuzzleRating(3000)
+            appSettingsRepository.updateTotalPuzzleCount(26_010)
         }
         emit(Unit)
     }.flatMapLatest {
